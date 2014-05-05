378720271fb4bce4d91dff4fa32003e87dd94386
JayfonLin <linjiafang33@163.com>
2014-05-05 15:29:13 +0800

    first commit

---
+++ Balloon/Balloon/Scenario1.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.DataTransfer;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Balloon
+{
+    
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Scenario1 : Page
+    {
+        DataTransferManager dtm;
+        public Scenario1()
+        {
+            this.InitializeComponent();
+        }
+
+        //进入时注册DataRequested事件
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            dtm= DataTransferManager.GetForCurrentView();
+            //创建event handler
+            dtm.DataRequested+= dtm_DataRequested;
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            dtm.DataRequested -= dtm_DataRequested;
+        }
+        void dtm_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            string textSource = "";
+            string source = TextSource.Text;
+            string textTitle = Title.Text;
+            string textDescription = MyDate.Date.ToString().Split('+')[0];
+            
+            DataPackage data = args.Request.Data;
+            data.Properties.Title = textTitle;
+            data.Properties.Description = textDescription;
+            textSource += "主题：";
+            textSource += textTitle;
+            textSource += "    ";
+            textSource += "时间：";
+            textSource += textDescription;
+            textSource += "   ";
+            textSource += "内容：";
+            textSource += source;
+
+            data.SetText(textSource);
+           
+        }
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        protected void ShowUIButton_Click(object sender, RoutedEventArgs e)
+        {
+            // If the user clicks the share button, invoke the share flow programatically.
+            DataTransferManager.ShowShareUI();
+        }
+    }
+}
